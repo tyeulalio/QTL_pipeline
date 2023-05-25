f36765e3413d07e168ea8756b1ef07b0de90dcdc
Tiffany Eulalio <tyeulalio@gmail.com>
2023-05-25 00:36:15 -0700

    adding colocalization script

---
+++ 03_colocalization/01_run_fastenloc.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# run fastenloc
+
+OUTDIR=$1
+QTL_FILE=$2
+GWAS_FILE=$3
+THREADS=$4
+
+
+# create the output directory if it doesn't exist
+mkdir -p $OUTDIR
+
+# run fastenloc 
+fastenloc \
+    -total_variants $NUM_GWAS_VARS \
+    -thread $THREADS \
+    -prefix "${OUTDIR}/fastqtl_" \
+    -eqtl $QTL_FILE \
+    -gwas $GWAS_FILE
