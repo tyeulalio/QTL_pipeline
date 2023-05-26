0434604ddedf9d7d68f20b8f2387148e96b78ac6
Tiffany Eulalio <tyeulalio@gmail.com>
2023-05-25 23:27:49 -0700

    added ptwas scripts

---
+++ 04_ptwas/01_compute_ptwas_weights.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# command line input
+DAPFILE=$1
+SBAMFILE=$2
+GENE=$3
+OUTDIR=$4
+
+# format output file
+mkdir -p ${OUTDIR}
+WEIGHTS_OUTDIR="${OUTDIR}/ptwas_weights/"
+mkdir -p ${WEIGHTS_OUTDIR}
+OUTPUTFILE="${WEIGHTS_OUTDIR}/${GENE}_ptwas_weights.txt"
+
+# run ptwas bulider
+ptwas_builder \
+    -f $DAPFILE \
+    -d $SBAMFILE \
+    -g $GENE \
+    -o $OUTPUTFILE
